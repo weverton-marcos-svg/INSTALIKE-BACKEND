df40426897f55bb5943f6d2a9600f04f7d83cf64
Weverton Marcos <wevertonmarcos.842@gmail.com.br>
2024-11-20 10:32:29 -0300

    Dia 01 - Criando um servidor

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+// Iniciando o express
+const app = express();
+app.listen(3000, () => {
+    console.log("Servidor escutando....");
+});
+
+app.get("/api",(req, res) => {
+    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+});
